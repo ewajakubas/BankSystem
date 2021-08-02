16862e44a1449de55f32de0120814d11539edd2b
Ewa <ewa.jakubas@wp.pl>
2021-08-02 19:20:39 +0200

    nowe zmiany squashowane

--- BankSystem/src/main/java/Account.java
+++ BankSystem/src/main/java/Account.java
@@ -2,7 +2,7 @@
 import java.math.BigDecimal;
 
 public class Account {
-    // pierwsza zmiana
+    // czwarta zm
     private final String firstName;
     private final String lastName;
     private BigDecimal balance;
@@ -12,12 +12,15 @@ public class Account {
  
 
     public Account(String firstName, String lastName, String login, String password, String iban){
+        // piata zmiana
         this.firstName = firstName;
         this.lastName = lastName;
         this.login = login;
         this.password = password;
         this.iban = iban;
         this.balance = new BigDecimal(0);
+
+
     }
  
     public String getFirstName(){
